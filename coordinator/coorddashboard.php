1c576eb4165b0de41b9f9765ce2f4a4b86c1b231
hablarekrek <hablarekrek@gmail.com>
2025-07-15 17:29:55 +0800

    fixed logout for coorddashboard.php

--- coordinator/coorddashboard.php
+++ coordinator/coorddashboard.php
@@ -572,7 +572,7 @@ tbody tr:hover {
 </nav>
       <hr class="separator" />
       <div class="logout">
- <a href="ojtform/logout.php" class="logout-link">
+ <a href="/ojtform/logout.php">
     <svg width="20" height="20" fill="none" stroke="currentColor" stroke-width="2"
          stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
       <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/>
