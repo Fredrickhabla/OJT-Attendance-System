b4ff19df1dbe0efc240bfb144916a011874e713e
hablarekrek <hablarekrek@gmail.com>
2025-07-29 08:56:34 +0800

    added paginator and download csv for dtr monitoring of coordinator

--- admin/view_attendancev2.php
+++ admin/view_attendancev2.php
@@ -43,6 +43,8 @@ $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet" />
   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css"/>
+  <link href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" rel="stylesheet">
+
 
   <style>
   * {
@@ -344,6 +346,8 @@ $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
     });
   });
 </script>
+<script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 <script src="/ojtform/autologout.js"></script>
 </body>
 </html>
